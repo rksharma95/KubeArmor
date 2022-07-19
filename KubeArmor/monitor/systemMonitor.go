920e69b96cc459f66f252554cd73f827738de437
achref ben saad <achref9612@gmail.com>
2022-07-19 10:02:37 +0000

    add security_path_unlink sys call

--- KubeArmor/monitor/systemMonitor.go
+++ KubeArmor/monitor/systemMonitor.go
@@ -295,7 +295,7 @@ func (mon *SystemMonitor) InitBPF() error {
 			}
 		}
 
-		sysKprobes := []string{"do_exit", "security_bprm_check", "security_file_open"}
+		sysKprobes := []string{"do_exit", "security_bprm_check", "security_file_open", "security_path_unlink"}
 
 		for _, sysKprobe := range sysKprobes {
 			kp, err := mon.BpfModule.LoadKprobe(fmt.Sprintf("trace_%s", sysKprobe))
@@ -407,7 +407,7 @@ func (mon *SystemMonitor) TraceSyscall() {
 					continue
 				}
 			} else if ctx.EventID == SysUnlink {
-				if len(args) != 1 {
+				if len(args) != 2 {
 					continue
 				}
 			} else if ctx.EventID == SysUnlinkAt {
